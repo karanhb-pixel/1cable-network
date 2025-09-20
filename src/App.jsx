89d6de76411cb307124af1d830d73d2e8a943675
karanhb-pixel <karan.9924304045@gmail.com>
2025-09-20 19:40:34 +0530

    add some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-
-import {Routes, Route } from 'react-router-dom';
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser, selectUser } from './store/authSlice';
-import User from './User/Users/User';
-import Home from './component/Home';
 import Navbar from './component/Navbar/navbar';
 import Footer from './component/footer/Footer';
-import Add_User from './User/Add_User/Add_User';
 import './App.css';
-import { Add_Wifi_plans } from './component/Wifi_plan/Add_Wifi_plans/Add_Wifi_plans';
-import { Show_Wifi_plans_2 } from './component/Wifi_plan/Show_Wifi_plans/Show_Wifi_plans_2';
 
-import { Ott_plan } from './component/Ott_plan/Ott_plan';
-import Edit_User from './User/Edit_User/Edit_User';
-import { Delete_User } from './User/Delete_User/Delete_User';
+// Dynamically import components that are not needed on initial page load
+const User = lazy(() => import('./User/Users/User'));
+const Home = lazy(() => import('./component/Home'));
+const Add_User = lazy(() => import('./User/Add_User/Add_User'));
+const Add_Wifi_plans = lazy(() => import('./component/Wifi_plan/Add_Wifi_plans/Add_Wifi_plans'));
+const Show_Wifi_plans_2 = lazy(() => import('./component/Wifi_plan/Show_Wifi_plans/Show_Wifi_plans_2'));
+const Ott_plan = lazy(() => import('./component/Ott_plan/Ott_plan'));
+const Edit_User = lazy(() => import('./User/Edit_User/Edit_User'));
+const Delete_User = lazy(() => import('./User/Delete_User/Delete_User'));
+
 function AppContent() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
@@ -34,16 +35,18 @@ function AppContent() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/user" element={<User />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/add_User" element={<Add_User />}/>
-        <Route path='/add_Wifi_plans' element={<Add_Wifi_plans/>} />
-        <Route path='/show_Wifi_plans_2' element={<Show_Wifi_plans_2/>} />
-        <Route path='/ott_plan' element={<Ott_plan/>} />
-        <Route path="/edit-user/:id" element={<Edit_User />} />
-        <Route path="/delete-user/:id" element={<Delete_User />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/user" element={<User />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/add_User" element={<Add_User />} />
+          <Route path='/add_Wifi_plans' element={<Add_Wifi_plans />} />
+          <Route path='/show_Wifi_plans_2' element={<Show_Wifi_plans_2 />} />
+          <Route path='/ott_plan' element={<Ott_plan />} />
+          <Route path="/edit-user/:id" element={<Edit_User />} />
+          <Route path="/delete-user/:id" element={<Delete_User />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   );
@@ -52,7 +55,5 @@ function AppContent() {
 function App() {
   return <AppContent />;
 }
-
-
 
 export default App;
